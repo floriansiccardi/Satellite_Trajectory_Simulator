c2896bbf88f65521e6d6b5cada9fe6c261a3e920
floriansiccardi <florian.siccardi.1@gmail.com>
2023-06-07 11:30:42 -0400

    Update simulator.py, planet.py, satellite.py

--- satellite.py
+++ satellite.py
@@ -1,1 +1,13 @@
+from random import randint
 
+
+class Satellite:
+
+    def __init__(self, name=''):
+
+        # Identification :
+        self.id = randint(0, 99999)
+        self.name = name
+
+    def __eq__(self, other):
+        return self.id == other.id
--- simulator.py
+++ simulator.py
@@ -1,1 +1,29 @@
+from planet import Planet
+from satellite import Satellite
 
+
+class Simulator:
+
+    def __init__(self, dt=1):
+        self.dt = dt
+
+        # Entités :
+        self.satellites = []
+        self.planets = []
+
+    def add(self, obj):
+        if type(obj) == Satellite:
+            self.satellites.append(obj)
+        elif type(obj) == Planet:
+            self.planets.append(obj)
+
+    def get(self, name):
+        for entities in [self.satellites, self.planets]:
+            for ent in entities:
+                if ent.name == name:
+                    return ent
+
+    def step(self):
+        for entities in [self.satellites, self.planets]:
+            for ent in entities:
+                ent.step()
